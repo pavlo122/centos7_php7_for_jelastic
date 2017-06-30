33dd0a1a60af86a3ca0efb6f55206a4a278b3a6c
pavlo122 <pavlofreemen@gmail.com>
2017-06-30 10:23:37 +0300

    Create update_php.sh

---
+++ update_php.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum -y install yum-plugin-replace php-common
+yum -y replace php-common --replace-with=php71w-common
+yum -y install php71w php71w-opcache php71w-gd php71w-mbstring php71w-mcrypt php71w-pdo php71w-soap php71w-xml php71w-bcmath
+yum -y install php71w-interbase php71w-dba php71w-mysqlnd php71w-common php71w-cli php71w-pgsql php71w-process php71w-tidy php71w-xml
+mv /etc/php.ini /etc/php.ini.back
+mv /etc/php.ini.rpmnew /etc/php.ini
